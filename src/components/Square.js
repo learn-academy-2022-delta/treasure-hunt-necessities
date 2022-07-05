9821dcea6bfc195c2c943c9a9b029e364412864f
Alex <ballou9013@gmail.com>
2022-07-05 14:59:08 -0700

    grid complete

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 
+
 class Square extends Component {
   render() {
     return(
       <>
+      <div className='square'> {this.props.value}</div>
+     
       </>
     )
   }
